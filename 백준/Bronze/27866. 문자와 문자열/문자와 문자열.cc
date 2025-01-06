3bd6d1795d6fbd3c7c051f625051df7ecdf28cb9
Superl3 <31559637+Superl3@users.noreply.github.com>
2025-01-07 03:40:13 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int index;
+    cin >> index;
+    cout << s[index-1]<<endl;
+    return 0;
+}
