5c309a9a90bccaf8cec177631e82fd93169b2c63
Superl3 <31559637+Superl3@users.noreply.github.com>
2025-01-15 01:15:12 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+
+	string str;
+	cin >> str;
+
+	int M = 1234567891;
+
+	unsigned long long r = 1, hash=0;
+	for (auto& letter: str) {
+		int num = letter - 'a' + 1;
+		hash += (num * r) % M;
+		r *= 31;
+	}
+	cout << hash % M << endl;
+	return 0;
+}
