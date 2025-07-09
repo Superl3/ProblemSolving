14c4f8d4571b4aa517c75a34a0c557ac648b2748
Superl3 <31559637+Superl3@users.noreply.github.com>
2025-07-09 09:50:10 +0900

    Time: 1 ms, Memory: 14.2 MB - LeetHub

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,15 @@
+#include<queue>
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp;
+        for(auto &num : nums) {
+            auto it = lower_bound(dp.begin(), dp.end(), num);
+            if(it == dp.end())
+                dp.push_back(num);
+            else
+                *it = num;
+        }
+        return dp.size();
+    }
+};
